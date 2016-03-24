0ddb6acd8880fc1d3bcccbef50e79d17ada0215d
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2016-03-24 17:52:14 +0000

    streaming to Riemann + basic Riemann config

--- src/weblog_riemann/core.clj
+++ src/weblog_riemann/core.clj
@@ -1,6 +1,10 @@
 (ns weblog-riemann.core
   (:require [clj-kafka.core :refer :all]
-            [clj-kafka.consumer.zk :refer :all])
+            [clj-kafka.consumer.zk :refer :all]
+            [cheshire.core :as json]
+            [clojure.string :as string]
+            [riemann.client :refer (tcp-client send-event)]
+            [clojure.set :refer (rename-keys)])
   (:import [java.lang String]))
 
 (def config {"zookeeper.connect" (System/getenv "ZOOKEEPER")
@@ -10,12 +14,40 @@
 
 (defn- parse
   [m]
-  (-> m .message String.))
+  (try
+    (-> m
+        .message
+        String.
+        (json/decode (fn [k] (keyword (string/replace k #"_" "-"))))
+        (update :content #(json/decode % (fn [k] (keyword (string/replace k #"_" "-"))))))
+    (catch Exception e nil)))
 
-(def xform (map parse))
+(defn- transform
+  [m]
+  (let [m (-> m
+              (merge (:content m))
+              (dissoc :content))]
+    (-> m
+       (rename-keys {:hostname :host})
+       (assoc :service "nginx requests")
+       (assoc :time (-> m :msec read-string int))
+       (update :body-bytes-sent (comp int read-string))
+       (update :request-time (comp int (partial * 1000) read-string))
+       (dissoc :msec :timestamp))))
+
+
+(def process (comp (map parse)
+                   (remove nil?)
+                   (map transform)))
+
+(defn- send
+  [r m]
+  (clojure.pprint/pprint m)
+  (send-event r m))
 
 (defn -main [& args]
-  (with-resource [c (consumer config)]
+  (with-resource [kafka (consumer config)]
     shutdown
-    (let [stream (create-message-stream c "syslogger" (default-decoder) (default-decoder))]
-      (run! println (eduction xform stream)))))
+    (let [riemann (tcp-client {:host (or (System/getenv "RIEMANN") "127.0.0.1")})
+          stream (create-message-stream kafka "syslogger")]
+      (run! (partial send riemann) (eduction process stream)))))
